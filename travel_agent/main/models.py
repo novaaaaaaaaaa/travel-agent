543c4ebfd4a03a2edcc2abad81acc65a99960e80
novaaaaaaaaaa <jackdickens048@gmail.com>
2024-03-04 12:30:21 +0000

    navbar and home page

---
+++ travel_agent/main/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
+# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20, blank=True)
+    address1 = models.CharField(max_length=200, blank=True)
+    address2 = models.CharField(max_length=200, blank=True)
+    city = models.CharField(max_length=50, blank=True)
+    postcode = models.CharField(max_length=20, blank=True)
+
+    def __str__(self) -> str:
+        return f'{self.user.username} has firstname: {self.user.first_name} and email address: {self.user.email}'
+
+
+#=============================================== Creation functions ========================================================
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+
+post_save.connect(create_profile, sender=User)
